63a04c1189d1137ce6c66724393192ba1ebe48bc
Karvendhan <karvsmech@gmail.com>
2020-01-04 09:03:07 +0530

    File modification: setup_class, teardown_class

--- test_module/test_class_doubleit.py
+++ test_module/test_class_doubleit.py
@@ -11,14 +11,18 @@ from file_folder import doubleit
 @pytest.mark.udemy_class
 class TestDoubleit(object):
 
-    numbers_file_template = 'testnums_template.txt'
-    numbers_file_testor = 'testnums.txt'
+    numbers_file_template = 'C:\\Users\\John\\PycharmProjects\\corey_schafer_oops/testnums_template.txt'
+    numbers_file_testor = 'C:\\Users\\John\\PycharmProjects\\corey_schafer_oops/testnums.txt'
 
-    def setup_method(self):
+    def setup_class(self):
         shutil.copy(TestDoubleit.numbers_file_template, TestDoubleit.numbers_file_testor)
 
     def teardown_class(self):
         os.remove(TestDoubleit.numbers_file_testor)
+
+    def test_doubleit_method1(self):
+        assert doubleit.double(19) == 38, 'method double fails'
+        assert doubleit.double(24) == 48, 'method double fails'
 
     def test_doublelines(self):
         doubleit.doublelines(TestDoubleit.numbers_file_testor)
@@ -27,11 +31,9 @@ class TestDoubleit(object):
         for old_val, new_val in zip(old_vals, new_vals):
             assert int(new_val) == int(old_val)*2
 
-    def test_doubleit_method1(self):
-        assert doubleit.double(19) == 38, 'method double fails'
-        assert doubleit.double(24) == 38, 'method double fails'
-
     def test_doubleit_type(self):
         with pytest.raises(TypeError):
             doubleit.double('hello')
+
+
 
